0b1c891027e8fdaf4f8a75735e3adc52c6dcc22d
Leksa <admin@petiteweb.ru>
2024-01-20 14:24:06 +0300

    Удалили лишние файлы из базовой сборки

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+  import => Получить кусок кода, кторый доступен при помощи экспорта внутри текущего файла
+  export => Сделать кусок кода (функция/класс/объект) доступным для использования в другом файле
+*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <h1>Hello!</h1>,
+  document.getElementById("root")
+)
