091a14e9a9581e13c57762002c73d1066cb7fa9c
Leksa <admin@petiteweb.ru>
2024-01-24 21:53:22 +0300

    Создание компонента Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { getArray } from "./utils/functions"
-
-import TestCard from "./components/TestCard/TestCard.jsx";
 import TextBlock from "./components/TextBlock/TextBlock";
 import textContent from "./assets/data/text.json";
+import Layout from "./components/Layout";
 
 const App = () => {
 
@@ -11,33 +9,32 @@ const App = () => {
         <section className="banner">
             <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, quod.</h1>
         </section>
-        <section className="second">
-            <h2>Точно подойдет для:</h2>
-            <div className="second__content">
-                {textContent
-                    .filter(item => item.block === "second")
-                    .map((item, i) => <TextBlock key={i} name={item.title} text={item.content} color="blueviolet"/>)
-                }
-            </div>
-        </section>
-        <section>
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, cumque.</h2>
-        </section>
-        <section className="bottom">
-            <h2>Точно подойдет для:</h2>
-            <div className="bottom__content">
-                {textContent
-                    .filter(item => item.block === "bottom")
-                    .map((item, i) => <TextBlock 
-                        key={i}
-                        name={item.title} 
-                        text={item.content}
-                        variant="colorize"
-                        // color="#89ffad"
-                    />)
-                }
-            </div>
-        </section>
+        <Layout 
+            name="Точно подойдет для:"
+            count={2}
+        >
+            {textContent
+                .filter(item => item.block === "second")
+                .map((item, i) => <TextBlock key={i} name={item.title} text={item.content} color="blueviolet"/>)
+            }
+        </Layout>
+        
+        <Layout/>
+        <Layout 
+            name="Точно подойдет для:"
+            size="300px"
+        >
+            {textContent
+                .filter(item => item.block === "bottom")
+                .map((item, i) => <TextBlock 
+                    key={i}
+                    name={item.title} 
+                    text={item.content}
+                    variant="colorize"
+                    // color="#89ffad"
+                />)
+            }
+        </Layout>
     </>
 }
 
